f70735fc52c2b4fa921ed46e2071f53ec4fd8cf6
mmarques-ssz <37246763+mmarques-ssz@users.noreply.github.com>
2024-10-10 21:17:01 -0300

    Add files via upload

---
+++ listaPOO_ex8/listaPOO_ex8/MaiorDeDois.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace listaPOO_ex8
+{
+    internal class MaiorDeDois
+    {
+        private int n1;
+        private int n2;
+        private int maior;
+        private string maiorExtenso;
+
+        public MaiorDeDois()
+        {
+            this.n1 = 0;
+            this.n2 = 0;
+        }
+
+        public MaiorDeDois(int n1, int n2)
+        {
+            this.n1 = n1;
+            this.n2 = n2;
+        }
+        public void setN1(int n1)
+        {
+            this.n1 = n1;
+        }
+        public void setN2(int n2)
+        {
+            this.n2 = n2;
+        }
+        public int getN1()
+        {
+            return this.n1;
+        }
+        public int getN2()
+        {
+            return this.n2;
+        }
+        public int getMaior()
+        {
+            return this.maior;
+        }
+        public string getMaiorExtenso()
+        {
+            return this.maiorExtenso;
+        }
+        public void descobrirMaior()
+        {
+            if (this.n1 > this.n2)
+            {
+                this.maior = this.n1;
+                this.maiorExtenso = "Primeiro";
+            }
+            else
+            {
+                this.maior = this.n2;
+                this.maiorExtenso = "Segundo";
+            }
+
+        }
+    }
+}
---
+++ listaPOO_ex8/listaPOO_ex8/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace listaPOO_ex8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MaiorDeDois mdd;
+            //mdd = new MaiorDeDois()
+            mdd = new MaiorDeDois(4, 5);
+
+            /*
+            Console.Write("Informe o 1º número: ");
+            mdd.setN1(int.Parse(Console.ReadLine()));
+            Console.Write("Informe o 2º número: ");
+            mdd.setN2(int.Parse(Console.ReadLine()));
+            */
+
+            mdd.descobrirMaior();
+
+            Console.WriteLine("O maior valor é o {0} que vale {1}",
+                mdd.getMaiorExtenso(), mdd.getMaior());
+            
+        }
+    }
+}
